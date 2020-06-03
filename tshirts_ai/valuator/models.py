2bce473169e2059d1bbe9253c6795fc8c1e81a08
YTK <yu.nakkano@gmail.com>
2020-06-03 22:30:57 +0900

    Implement Tshirt model in /valuator/models.py

--- tshirts_ai/valuator/models.py
+++ tshirts_ai/valuator/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from PIL import Image
+import io
+import base64
+
+# Initialize graph variable to load Prediction model.
+graph = tf.compat.v1.get_default_graph()
+
+
+class Tshirt(models.Model):
+    image = models.ImageField(upload_to='photos')
+    IMAGE_SIZE = 224  # 画像サイズ
+    MODEL_FILE_PATH = './carbike/ml_models/vgg16_transfer.h5'  # Model file
+    classes = [
+        "0300-0800", "0800-1300", "1300-1800", "1800-2500",
+        "2500-3200", "3200-4000", "4000-5000", "5000-"
+    ]
+    num_classes = len(classes)
+
+    def predict(self):
+        model = None
+        global graph  # graph is called every time to use the same VGG16 model
+        with graph.as_default():
+            model = load_model(self.MODEL_FILE_PATH)
+
+            img_data = self.image.read()
+            # For Pillow to open the image file, convert data to BytesIO
+            img_bin = io.BytesIO(img_data)
+            image = Image.open(img_bin)
+            image = image.convert("RGB")
+            image = image.resize((self.IMAGE_SIZE, self.IMAGE_SIZE))
+            data = np.asarray(image) / 255.0
+            X = []
+
+            X.append(data)
+            X = np.array(X)
+            result = model.predict([X])[0]  # Obtain the first result
+            predicted = result.argmax()
+            percentage = int(result[predicted] * 100)
+            # print(self.classes[predicted], percentage)
+            return self.classes[predicted], percentage
+
+    def image_source(self):
+        with self.image.open() as img:
+            base64_img = base64.b64encode(img.read()).decode()
+
+            return 'data:' + img.file.content_type + ';base64,' + base64_img
