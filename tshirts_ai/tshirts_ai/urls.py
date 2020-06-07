f2c32bf71d3393d03afa8a912fe854057e2f224c
YTK <yu.nakkano@gmail.com>
2020-06-07 23:15:57 +0900

    Added static files path to urls.py to handle media files.

--- tshirts_ai/tshirts_ai/urls.py
+++ tshirts_ai/tshirts_ai/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'valuator'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('valuator/', include('valuator.urls')),
     path('cam_valuator/', include('cam_valuator.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
