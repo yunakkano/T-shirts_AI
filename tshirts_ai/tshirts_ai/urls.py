40434dcaab9e1772b4a7cc6abb5af4a7c4da88fd
YTK <yu.nakkano@gmail.com>
2020-06-08 21:08:59 +0900

    Added routing to views.py at root level.

--- tshirts_ai/tshirts_ai/urls.py
+++ tshirts_ai/tshirts_ai/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 app_name = 'valuator'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('valuator/', include('valuator.urls')),
     path('cam_valuator/', include('cam_valuator.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
