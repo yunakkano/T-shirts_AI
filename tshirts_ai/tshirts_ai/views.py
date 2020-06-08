a26e08d3c70e54af3a98231f51422345025b84a5
YTK <yu.nakkano@gmail.com>
2020-06-08 21:08:35 +0900

    Created views.py for root level controller.

---
+++ tshirts_ai/tshirts_ai/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from valuator.models import Tshirt
+
+
+def index(request):
+    # template = loader.get_template('valuator/index.html')
+    tshirts = Tshirt.objects.order_by('-saved_at')
+    context = {'tshirts': tshirts}
+    return render(request, 'index.html', context)
+
+
+def tshirt_detail(request, tshirt_id):
+    tshirt = get_object_or_404(Tshirt, pk=tshirt_id)
+    return render(request, 'tshirt_detail.html', {'tshirt': tshirt })
+
