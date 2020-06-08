1ebbdc4e0b33018239e5ef740b303b528acc1eae
YTK <yu.nakkano@gmail.com>
2020-06-08 21:05:11 +0900

    Added routing to tshirt_detail view

--- tshirts_ai/valuator/urls.py
+++ tshirts_ai/valuator/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 
 app_name = 'valuator'
@@ -6,4 +7,6 @@ app_name = 'valuator'
 urlpatterns = [
     path('', views.index, name='index'),
     path('valuate/', views.valuate, name='valuate'),
+    re_path(r'valuate/(?P<tshirt_id>[0-9]+)/$', views.tshirt_detail,
+        name="tshirt_detail")
 ]
