8f5ede88e41facb8b49eefac3fec046f8fd41cdd
YTK <yu.nakkano@gmail.com>
2020-06-09 21:39:41 +0900

    Modified valuator/views.py
    - same as the modification to cam_valuator/views.py

--- tshirts_ai/valuator/views.py
+++ tshirts_ai/valuator/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
-from django.http import HttpResponse
-from django.template import loader
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.core.files import File
+
+from datetime import datetime as dt
 from .forms import PhotoForm
 from .models import Tshirt
+from valuator.models import User
+from io import BytesIO
+from PIL import Image
+import base64
+import re
+import os
 
 
 def index(request):
@@ -21,26 +30,55 @@ def tshirt_detail(request, tshirt_id):
 
 
 def valuate(request):
-    # POST request = file upload from upload button
     if not request.method == 'POST':
         return
         redirect('valuator:index')
-    # If file upload request comes normally,
-    # then create PhotoForm instance with file object.
     form = PhotoForm(request.POST, request.FILES)
     if not form.is_valid():
         raise ValueError("Form invalid")
 
     tshirt = Tshirt(image=form.cleaned_data['image'])
+    image_str = tshirt.image_source()
+    imtype = re.search("^data:image/(jpeg|png|gif|jpg);base64,", image_str).group(1)
+    image_data = re.sub("^data:image/(jpeg|png|gif|jpg);base64,", "", image_str)
+    image_data = base64.b64decode(image_data)
+    image_data = BytesIO(image_data)
+    im = Image.open(image_data)
+    imName = str(dt.now()).replace(" ", "_").replace(":", "") + '.' + imtype
+    im.save('media/tmp/' + imName, imtype)
+    with open('media/tmp/' + imName, 'rb') as reopen:
+        django_file = File(reopen)
+        tshirt = Tshirt(image=django_file)
     # Call the method to predict
-    predicted, percentage = tshirt.valuate()
+        predicted, percentage = tshirt.valuate()
 
-    template = loader.get_template('valuator/result.html')
     context = {
         'predicted':    predicted,
         'percentage':   percentage,
-        'tshirt_name':  tshirt.image.name,
-        'tshirt_data':  tshirt.image_source(),
+        'tshirt_name':  str(tshirt.image.name).split('/')[-1],
+        'tshirt_data':  image_str,
     }
+    return render(request, 'valuator/result.html', context)
 
-    return HttpResponse(template.render(context, request))
+
+def save_result(request):
+    if not request.method == 'POST':
+        return redirect('index')
+    if request.POST['saving'] == 'True':
+        with open('media/tmp/' + request.POST['tshirt_name'], 'rb') as reopen:
+            django_file = File(reopen)
+            django_file.name = django_file.name.replace('media/tmp/','')
+            tshirt = Tshirt(
+                image=django_file,
+                price_range=request.POST['predicted'],
+                comment=request.POST['comment'],
+                saved_at=dt.now(),
+                confidence=request.POST['percentage'],
+                user=get_object_or_404(User, pk=2)
+                )
+            tshirt.save()
+        os.remove('media/tmp/' + request.POST['tshirt_name'])
+        return redirect('index')
+    else:
+        os.remove('media/tmp/' + request.POST['tshirt_name'])
+        return redirect('valuator:index')
