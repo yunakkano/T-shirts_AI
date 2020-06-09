2e0187732b20d0bcbee46bd41e380e733528cd49
YTK <yu.nakkano@gmail.com>
2020-06-09 21:24:14 +0900

    Determined to delete custom User class. Instead use built-in User class in Django

--- tshirts_ai/valuator/admin.py
+++ tshirts_ai/valuator/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tshirt, User
+from .models import Tshirt
 
 admin.site.register(Tshirt)
-admin.site.register(User)
+# admin.site.register(User)
--- tshirts_ai/valuator/models.py
+++ tshirts_ai/valuator/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -12,29 +12,19 @@ import base64
 graph = tf.compat.v1.get_default_graph()
 
 
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-
-    def __str__(self):
-        return "%s %s" % (self.first_name, self.last_name)
-
-
 class Tshirt(models.Model):
     image = models.ImageField(upload_to='Photos/')
-    # image = models.ImageField()
     price_range = models.CharField(max_length=50)
     comment = models.TextField()
-    saved_at = models.DateField()
+    saved_at = models.DateTimeField()
     confidence = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     IMAGE_SIZE = 224  # 画像サイズ
     MODEL_FILE_PATH = './valuator/ml_models/vgg16_Tshirts.h5'  # Model file
     classes = [
-        "0300-0800", "0800-1300", "1300-1800", "1800-2500",
-        "2500-3200", "3200-4000", "4000-5000", "5000-"
+        "300円〜800円", "800円〜1300円", "1300円〜1800円", "1800円〜2500円",
+        "2500円〜3200円", "3200円〜4000円", "4000円〜5000円", "5000円以上"
     ]
     num_classes = len(classes)
 
@@ -58,13 +48,11 @@ class Tshirt(models.Model):
             result = model.predict([X])[0]  # Obtain the first result
             predicted = result.argmax()
             percentage = int(result[predicted] * 100)
-            # print(self.classes[predicted], percentage)
             return self.classes[predicted], percentage
 
     def image_source(self):
         with self.image.open() as img:
             base64_img = base64.b64encode(img.read()).decode()
-
             return 'data:' + img.file.content_type + ';base64,' + base64_img
 
     def __str__(self):
