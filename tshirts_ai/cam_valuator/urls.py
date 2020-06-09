006ca1caa66918e8af2ed6dc9a03730538836376
YTK <yu.nakkano@gmail.com>
2020-06-09 21:31:56 +0900

    Added routing to save_result method in views.py for valuator / cam_valuator apps

--- tshirts_ai/cam_valuator/urls.py
+++ tshirts_ai/cam_valuator/urls.py
@@ -6,4 +6,5 @@ app_name = 'cam_valuator'
 urlpatterns = [
     path('', views.index, name='index'),
     path('cam_valuate/', views.cam_valuate, name='cam_valuate'),
+    path('save_result/', views.save_result, name='save_result')
 ]
--- tshirts_ai/valuator/urls.py
+++ tshirts_ai/valuator/urls.py
@@ -7,6 +7,7 @@ app_name = 'valuator'
 urlpatterns = [
     path('', views.index, name='index'),
     path('valuate/', views.valuate, name='valuate'),
+    path('save_result/', views.save_result, name='save_result'),
     re_path(r'valuate/(?P<tshirt_id>[0-9]+)/$', views.tshirt_detail,
         name="tshirt_detail")
 ]
