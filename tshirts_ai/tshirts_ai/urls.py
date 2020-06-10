d9a5187178f672372188b5854e17b5f0cf5521ee
YTK <yu.nakkano@gmail.com>
2020-06-10 15:56:30 +0900

    Added routing to register app, login page and logout page

--- tshirts_ai/tshirts_ai/urls.py
+++ tshirts_ai/tshirts_ai/urls.py
@@ -20,11 +20,16 @@ from django.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from django.contrib.auth.views import LogoutView
+from register import views as v
 
 app_name = 'valuator'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('register/', v.SignUp.as_view(), name="register"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('', include("django.contrib.auth.urls")),
     path('valuator/', include('valuator.urls')),
     path('cam_valuator/', include('cam_valuator.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
