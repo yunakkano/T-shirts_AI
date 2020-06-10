737baf031c6ba27f013070292297ee3cd527acc1
YTK <yu.nakkano@gmail.com>
2020-06-10 15:55:47 +0900

    Added codes to capture current user information in tshirts_ai/views.py

--- tshirts_ai/tshirts_ai/views.py
+++ tshirts_ai/tshirts_ai/views.py
@@ -4,12 +4,14 @@ from valuator.models import Tshirt
 
 
 def index(request):
-    tshirts = Tshirt.objects.order_by('-saved_at')
-    context = {'tshirts': tshirts}
-    return render(request, 'index.html', context)
+    current_user = request.user
+    tshirts = Tshirt.objects.all().filter(user_id=current_user.id).order_by('-saved_at')
+    context = {'current_user': current_user, 'tshirts': tshirts}
+    return render(request, 'top.html', context)
 
 
 def tshirt_detail(request, tshirt_id):
-    tshirt = get_object_or_404(Tshirt, pk=tshirt_id)
+    current_user = request.user
+    tshirt = get_object_or_404(Tshirt, pk=tshirt_id).filter(user_id=current_user.id).order_by('-saved_at')
     return render(request, 'tshirt_detail.html', {'tshirt': tshirt })
 
