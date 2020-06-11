e2923ed415de71b84f2d39685d57b837dbd4f4c6
YTK <yu.nakkano@gmail.com>
2020-06-11 17:16:37 +0900

    Added import codes in wsgi.py for pymysql to work with gunicorn

--- tshirts_ai/tshirts_ai/wsgi.py
+++ tshirts_ai/tshirts_ai/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import pymysql
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tshirts_ai.settings')
 
