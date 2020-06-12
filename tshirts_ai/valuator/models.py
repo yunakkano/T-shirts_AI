dbd0faa061b5eace7cd04b1b8b2ad3b36bf51b1a
YTK <yu.nakkano@gmail.com>
2020-06-12 18:04:29 +0900

    Updated codes in relation to loading Keras' NN model files

--- tshirts_ai/valuator/models.py
+++ tshirts_ai/valuator/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from tensorflow import keras
 from PIL import Image
+from tshirts_ai.settings import BASE_DIR
+import os
 import io
 import base64
-
-# Initialize graph variable to load Prediction model.
-graph = tf.compat.v1.get_default_graph()
-
 
 class Tshirt(models.Model):
     image = models.ImageField(upload_to='Photos/')
@@ -20,7 +18,7 @@ class Tshirt(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     IMAGE_SIZE = 224  # 画像サイズ
-    MODEL_FILE_PATH = './valuator/ml_models/vgg16_Tshirts.h5'  # Model file
+    MODEL_FILE_PATH = os.path.join(BASE_DIR, 'valuator/my_model')
     classes = [
         "300円〜800円", "800円〜1300円", "1300円〜1800円", "1800円〜2500円",
         "2500円〜3200円", "3200円〜4000円", "4000円〜5000円", "5000円以上"
@@ -29,25 +27,24 @@ class Tshirt(models.Model):
 
     def valuate(self):
         model = None
-        global graph  # graph is called every time to use the same VGG16 model
-        with graph.as_default():
-            model = load_model(self.MODEL_FILE_PATH)
+        # global graph  # graph is called every time to use the same VGG16 model
+        # with graph.as_default():
+        model = keras.models.load_model(self.MODEL_FILE_PATH)
+        img_data = self.image.read()
+        # For Pillow to open the image file, convert data to BytesIO
+        img_bin = io.BytesIO(img_data)
+        image = Image.open(img_bin)
+        image = image.convert("RGB")
+        image = image.resize((self.IMAGE_SIZE, self.IMAGE_SIZE))
+        data = np.asarray(image) / 255.0
+        X = []
 
-            img_data = self.image.read()
-            # For Pillow to open the image file, convert data to BytesIO
-            img_bin = io.BytesIO(img_data)
-            image = Image.open(img_bin)
-            image = image.convert("RGB")
-            image = image.resize((self.IMAGE_SIZE, self.IMAGE_SIZE))
-            data = np.asarray(image) / 255.0
-            X = []
-
-            X.append(data)
-            X = np.array(X)
-            result = model.predict([X])[0]  # Obtain the first result
-            predicted = result.argmax()
-            percentage = int(result[predicted] * 100)
-            return self.classes[predicted], percentage
+        X.append(data)
+        X = np.array(X)
+        result = model.predict([X])[0]  # Obtain the first result
+        predicted = result.argmax()
+        percentage = int(result[predicted] * 100)
+        return self.classes[predicted], percentage
 
     def image_source(self):
         with self.image.open() as img:
---
+++ tshirts_ai/valuator/my_model/saved_model.pb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e4183778b80ae2b76a09229640d353b9ab7ce27ff58e1c4862e235dac5db498
+size 378889
